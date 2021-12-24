3bee5608e6fca824423c8a7af9e69364094a38f2
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-12-25 02:58:31 +0530

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://ieeenitdgpbackend.herokuapp.com/api",
+  baseURL: "https://ieeesbnitdgp-api.herokuapp.com/api/",
   headers: {
     "Content-type": "application/json"
   },
