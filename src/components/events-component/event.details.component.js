511cf8f014f55bde4595761cbe1c6827c6f298eb
SantamRC <santamdev404@gmail.com>
2021-12-27 00:19:56 +0530

    Added Static Cards(Frontend only)

--- src/components/events-component/event.details.component.js
+++ src/components/events-component/event.details.component.js
@@ -1,104 +1,23 @@
 import React, { Component } from "react";
-import PostDataService from "../../services/post.service"
 import Fade from 'react-reveal/Fade'
 import "./events.css"
 import { Link, Redirect, Switch, Route } from "react-router-dom";
 
 
 class EventDetails extends Component {
-constructor(props) {
-    super(props);
-    this.getEvent = this.getEvent.bind(this);
 
-    this.state = {
-      currentEvent: {
-        id: null,
-        title: "",
-        slug: "google-cloud-study-jams",
-        updated_on: "2021-02-25T20:47:27.447444Z",
-        content: "",
-        image: "",
-        image_1: "",
-        image_2: "",
-        image_3: "",
-        created_on: "",
-        status: 0,
-        event_date: ""
-      },
-      message: "",
-      retrieved: false
-    };
-  }
-
-  componentDidMount() {
-
-    this.getEvent(this.props.match.params.id);
-
-  }
-
-
-
-	  getEvent(id){
-	  	PostDataService.get(id)
-	  	.then(response => {
-	  		this.setState({
-	          currentEvent: response.data,
-	          retrieved: true
-	        });
-	  		console.log(response.data);
-	  	})
-	  	.catch(e => {
-	  		console.log(e);
-	  	});
-	  }
-	  refreshList() {
-	    this.getEvent(this.props.match.params.id);
-	    this.setState({
-	    	retrieved: true
-	    });
-	  }
-
-  
   render() {
       return (
         <div className="text-center main-container">
         	<div className="jumbotron banner border-0 rounded-0">
         		<h1 className="display-3 text-light"> Events </h1>
         	</div>
-        	
 			  <div className="row row-details">
-			  	{this.state.retrieved ? "" : (
-			  		<div className="display-3 p-3">
-			  			Loading...
-			  		</div>
-			  		)}
-			  	
-			  		<div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 card card-details">
-			  			<div className="image-wrapper image-wrapper-details">
-			  				<img className="card-img-top event-img-details" src={this.state.currentEvent.image} />
-			  			</div>
-					    <div className="card-body">
-					      <h1 className="text-center heading lead">{this.state.currentEvent.title}</h1>
-					      <p className="card-text"> {this.state.currentEvent.content} </p>
-					    </div>
-					    <div className="row grid-img-wrapper">
-					    	<div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 grid-img">
-					    		<img src={this.state.currentEvent.image_1} />
-					    	</div>
-					    	<div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 grid-img">
-					    		<img src={this.state.currentEvent.image_2} />
-					    	</div>
-					    	<div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 grid-img">
-					    		<img src={this.state.currentEvent.image_3} />
-					    	</div>
-
-					    </div>
-			  		</div>
-
+				<div className="display-3 p-3">
+					Loading...
+				</div>
 			  </div>
-			
         </div>
-          
       )
   }
 }
---
+++ src/data/events.js
@@ -0,0 +1,130 @@
+const events = [
+  {
+    title: "Qriosity 2.0",
+    content:
+      "Tired of your monotonous life? Fed up with your caged existence? Sick of watching overrated Netflix and owned by pros on Valorant?\r\n\r\nWorry not. IEEE SB NIT Durgapur brings you QRIOSITY 2.0 - the quiz to remedy all your lockdown woes.\r\nThis lockdown get rid of the boredom and awaken the decoder inside you! Make your way through intriguing algorithms and tricky tech questions which will finally lead you to a passcode that can solve the Enigma! Save the world in an alternate dimension and become the hero the people so desperately need!\r\n\r\nThis September, we are here to activate your grey matter and get your neurons firing. So, get ready to play your own Imitation Game with IEEE SB NIT Durgapur presents - QRIOSITY 2.0.",
+    image: "qriosity.png",
+    image_1: "qriosity2.png",
+    image_2: "qrio3.png",
+    image_3: "qrio.png",
+    event_date: "2021-09-12",
+  },
+  {
+    title: "Matlab Mastery",
+    content:
+      "In spite of having scores of complex mathematical formulae, technical know-how, and theoretical jargons at your disposal, every creative idea realizes its true purpose only through repeated trial and testing, and this is precisely where MATLAB comes off as one of the most useful tools for the engineering community.\r\n\r\nIEEE SB NIT Durgapur is here to inspire your ingenuity with MATLAB Mastery 2.0, an interactive free workshop with weekly classes to get you acquainted with every nook and cranny of the software. Don't forego this opportunity of decorating your CV with shades of technical prowess!",
+    image: "matlab.png",
+    image_1:
+      "Screenshot_244.png",
+    image_2:
+      "Screenshot_246.png",
+    image_3:
+      "Screenshot_248.png",
+    event_date: "2021-04-07",
+  },
+  {
+    title: "Collaboration with with the University of British Columbia, Canada",
+    content:
+      "IEEE SB NIT Durgapur is organizing a virtual International Field Trip for the IEEE Student Branch, University of British Columbia\r\nNIT Durgapur, Research, India as a Job Destination, Internships\r\nTime: Mar 11, 2021, 06:30 PM Pacific Time (the US and Canada)",
+    image: "ubc.png",
+    image_1:
+      "Screenshot_from_2021-03-12_08-50-01.png",
+    image_2:
+      "WhatsApp_Image_2021-04-14_at_11.55.47_PM.jpeg",
+    image_3:
+      "WhatsApp_Image_2021-04-14_at_11.55.48_PM.jpeg",
+    event_date: "2021-03-11",
+  },
+  {
+    title: "Internship Bootcamp 3",
+
+    content:
+      "Having treaded through the beginning of your college life, you must have got familiar with the hectic academics and overwhelming assignments, with the aim to boost your career with a perfect headstart. But are you familiar with the things actually required for a perfect resume? Are you aware of the infinite opportunities you can grab through these 4 years of college?\r\n\r\nWorry not, the IEEE Student Branch NIT Durgapur has got you covered ! Presenting to you INTERNSHIP BOOTCAMP 3.0, a session to get you familiar with the world of resumes, internships and opportunities. Get answers to all those burning questions such as where to apply and how to apply. Be furnished with information on the exciting foreign internships, research prospects and more from Experience Talks given by students who have already interned at reputed institutions.\r\n\r\nAlso, be part of a hands-on workshop to learn how to frame your resume, how to prepare your SOP and tips for interviews and the overall associated application process!\r\n\r\nBecause the sooner you get to know stuff in college, the better you perform after college.",
+    image: "IBC.png",
+    image_1:
+      "Screenshot_253.png",
+    image_2:
+      "Screenshot_255.png",
+    image_3:
+      "Screenshot_254.png",
+
+    event_date: "2021-02-15",
+  },
+  {
+    title: "IEEE DAY",
+
+    content:
+      "6th October, celebrated as IEEE day, is round the corner and it's yet another day for celebrating engineers, technical innovations and ideas.\r\nIEEE SB NIT Durgapur brings to you an interactive session by keynote speakers who would share their research experience and significance of IEEE in shaping their careers in academia. This session shall sight to provide paramount guidance for all the budding researchers and shall give you a chance to put up your queries in front of them. We will be streaming live on YouTube for two talk sessions.\r\nTalk session 1: 3.45 pm to 5 pm\r\nTalk session 2: 5.15 pm to 6.30 pm\r\nSo mark your dates and don't miss your chance to participate in this unique opportunity of interacting with leaders and young professionals who have made it big.",
+    image:
+      "ieeeday0.png",
+    image_1:
+      "IEEE_day.png",
+    image_2:
+      "ieeeday2.png",
+    image_3:
+      "ieeeday0_ORMMih0.png",
+
+    event_date: "2020-10-06",
+  },
+  {
+    title: "Polaris",
+
+    content:
+      "Humans beings have always been morbidly curious creatures – it’s why they continue to thrive in this modern era, so in a way, we have received the spirit of research from our ancestors through heredity.\r\nBut it has been seen that there is a dearth of enthusiasm among many engineering students to pursue a career in research and innovation, which can be attributed to a host of misconceptions and a general lack of exposure to the multifarious work opportunities in various research disciplines.\r\nTo dispel uncertainty about these opportunities, IEEE SB NITD brings forth POLARIS, a video series to introduce you to the world of exploration and scrutiny. We bring to you interview sessions with speakers having a range of diverse accomplishments in various positions. This series aims to challenge ancient assumptions and misconceptions and highlight the towering opportunities the field of RESEARCH offers.",
+    image:
+      "polaris0.png",
+    image_1:
+      "polaris1.png",
+    image_2:
+      "polaris.png",
+    image_3:
+      "polaris0_p4yO42E.png",
+
+    event_date: "2020-05-14",
+  },
+  {
+    title: "SDV Workshop",
+
+    content:
+      "In this era of advancement and competing efficiency where society expects autonomous vehicles to be held to a higher standard than human drivers, IEEE Student Branch, NIT Durgapur in collaboration with the Society of Automotive Engineers organized a two-day workshop on 31 August and 1 September 2019 to introduce students to the world of autonomous driving.\r\nThe Day 1of the workshop started with an introduction to the history of self-driving cars and the need to introduce them into this gradually cementing world of intelligence by Animesh Datta, Chair, IEEE SB NIT Durgapur.\r\nThe workshop was then followed by an introduction to algorithms and data structures that are essential keys for autonomous driving by Amitava Das, from the department of electronics and communication engineering.\r\nHands on-session on the development of the prototype on the Second day of the workshop began with a prelude to Arduino programming and then covered certain vital concepts.\r\nEach team was provided with a kit that consisted of all the obligatory components for the development of the prototype and a brief demonstration of each component was adeptly provided. The workshop concluded with a presentation given by Abhishek Singh, from the Department of Computer Science and Engineering on Machine learning. The workshop was a huge success for both the bodies and gathered massive attention towards this crucial upcoming reckon.",
+    image: "SDv1.jpg",
+    image_1: "SDv4.jpg",
+    image_2: "SDv3.jpg",
+    image_3: "SDv2.jpg",
+
+    event_date: "2019-12-18",
+  },
+  {
+    title: "IntelliSys",
+
+    content:
+      "IntelliSys: Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.\r\n\r\nIEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response and helping us in our endeavors to spread the IEEE vision - Advancing technology for humanity.",
+    image:
+      "intellisys1_bwWfYLi.jpg",
+    image_1:
+      "intellisys2_h6QrvS0.jpg",
+    image_2:
+      "intellisys3_sIT7AeR.jpg",
+    image_3:
+      "intellisys4_wYHckiK.jpg",
+
+    event_date: "2019-05-04",
+  },
+  {
+    title: "The Art of Scientific Writing",
+
+    content:
+      "IEEE Student Branch NIT DURGAPUR conducted the “The Art of Scientific Writing“ on 31st January 2019 at the C.V.RAMAN Auditorium, New Academic Building. The response was overwhelming and numerous students irrespective of their branch attended it.\r\n\r\nWe were privileged to have with us, Dr. Aniruddha Chandra, associate professor of Electronics and Communication Engineering (ECE) Department and our newly appointed faculty advisor.\r\n\r\nHe started by sharing the experiences and works of his research undergraduate students who are extremely successful in their lives currently. He discussed in detail the format in which a scientific paper should be written and the various aspects related to it. He also guided in selecting the right mentors (both internal and external) and the professors of an institute under whom one is going to carry out their research work. He also stressed on the importance of funding and sponsorships for carrying out the research.\r\n\r\nHe was followed by Aniq Ur Rahman, Chair IEEE Student Branch NIT Durgapur, who shared the journey of publishing his research work. He shared some valuable key points like the importance of maintaining a research notebook, organizing one’s thoughts, getting constant feedback and improving the work until its ready.\r\n\r\nIn a nutshell, the event was a great success involving sensational speakers and an interactive audience.",
+    image:
+      "aosw3_uXKwHNF.jpg",
+    image_1:
+      "aosw2_W4yg8Er.jpg",
+    image_2: "aosw1.jpg",
+    image_3:
+      "aosw3_uXKwHNF_ftO9OBG.jpg",
+
+    event_date: "2019-03-04",
+  },
+];
+
+export default events;
